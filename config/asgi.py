a1f0e72b746af57ee7e59da1732070a36cb9e7fb
Nicusor <lino479Nick@mail.ru>
2022-12-29 22:45:46 +0300

    6th commit

--- config/asgi.py
+++ config/asgi.py
@@ -1,14 +1,16 @@
+import django
+import chat.routing
+from django.core.asgi import get_asgi_application
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
-
-import chat.routing
-
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+django.setup()
+
+
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
     'websocket': AllowedHostsOriginValidator(
